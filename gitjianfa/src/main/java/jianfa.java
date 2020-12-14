ca696e5275c8cdca9c596d0fe3a01651aa1a4608
yq919398 <y17726114459.@163.com>
2020-12-14 16:57:44 +0800

    创建

---
+++ gitjianfa/src/main/java/jianfa.java
@@ -0,0 +1,9 @@
+public class jianfa {
+    public static void main(String[] args) {
+        System.out.println("华山剑法");
+        System.out.println("华山剑法");
+        System.out.println("华山剑法");
+        System.out.println("华山剑法");
+        System.out.println("华山剑法");
+    }
+}
